a8c7c0a3a481c7097eda66c2ef4078c7d5d53ba7
Oliver Kang <ivorkang729@gmail.com>
2021-10-30 22:19:19 +0800

    update

--- src/main/java/com/example/demo/controller/MyAPIController.java
+++ src/main/java/com/example/demo/controller/MyAPIController.java
@@ -11,31 +11,28 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.demo.anno.MyActionLog;
 import com.example.demo.anno.MyAuthRequired;
 
 @RestController
 public class MyAPIController {
 	
-	private Map<String, List<String>> roleAuthSettings;
 	
-	@PostConstruct
-	private void postConstruct() {
-		roleAuthSettings = new HashMap<>();
-		roleAuthSettings.put("ROLD_ADMIN", Arrays.asList("query", "delete"));
-		roleAuthSettings.put("ROLD_USER", Arrays.asList("query"));
-	}
 	
+	@MyActionLog(action = "ACT_INITPAGE")
 	@GetMapping("/initPage")
 	public String initPage(@RequestParam() String role) {
 		return "Do Init Page, User has role: " + role;
 	}
 	
+	@MyActionLog(action = "ACT_QUERY")
 	@MyAuthRequired(authId = "query")
 	@GetMapping("/query")
 	public String query(@RequestParam() String role) {
 		return "Do Query, User has role: " + role;
 	}
 	
+	@MyActionLog(action = "ACT_DELETE")
 	@MyAuthRequired(authId = "delete")
 	@GetMapping("/delete")
 	public String delete(@RequestParam String role) {
