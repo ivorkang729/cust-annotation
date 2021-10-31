314d0579626d904ba19947bbc123f02597438b60
Oliver Kang <ivorkang729@gmail.com>
2021-10-31 12:31:22 +0800

    update

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,7 +3,6 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "com.example.demo")
@@ -14,3 +13,4 @@ public class DemoApplication {
 	}
 
 }
+
--- src/main/java/com/example/demo/controller/MyAPIController.java
+++ src/main/java/com/example/demo/controller/MyAPIController.java
@@ -1,12 +1,5 @@
 package com.example.demo.controller;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import javax.annotation.PostConstruct;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
